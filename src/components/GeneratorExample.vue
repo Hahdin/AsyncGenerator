78e5d5151ab00f7d6ac7f1a36126adcae71cb32a
Mark Foster <28118038+Hahdin@users.noreply.github.com>
2021-02-28 09:58:28 -0700

    cleanup

--- src/components/GeneratorExample.vue
+++ src/components/GeneratorExample.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
     <div class="jumbotron title-box">
-      <h1 class="mytitle">Async Generator Progress Bar
+      <h1 class="mytitle engrave">Async Generator Progress Bar
       </h1>
       <hr />
       <div class="progress container mybar-container" style="width:80%">
@@ -9,8 +9,8 @@
           {{ precise(state.loaded, 4) }}%
         </div>
       </div>
-      <div id='msg' />
-      <div id="loadmsg" hidden="true">
+      <div id='msg' class="engrave" />
+      <div id="loadmsg" class="engrave" hidden="true">
         Loading... please wait.
         <span id="spinspan-small" class="spinner-border spinner-border-sm text-success"></span>
       </div>
@@ -23,7 +23,7 @@ window.onload = () => {
   document.documentElement.style.setProperty('--percentage', state.loaded);
 };
 
-import { defineProps, onMounted, reactive } from 'vue'
+import { defineProps, onMounted, reactive, resolveDirective } from 'vue'
 defineProps({
   // msg: String
 })
@@ -64,6 +64,7 @@ const gen = () => {
   (async () => {
     for await (const val of progressbar) {
       state.loaded = (state.max - val.count) / state.max * 100;
+      document.documentElement.style.setProperty('--pct-color', getRgb(state.loaded));
       document.documentElement.style.setProperty('--percentage', state.loaded);
       const bar = document.getElementById('bar');
       bar.style.width = `${state.loaded}%`;
@@ -73,41 +74,69 @@ const gen = () => {
   })();
 }
 
+const getRgb = percent => {
+  const op = 0.8;
+  const changeColor = 100 / 6;
+  if (percent < changeColor) {
+    let pct = percent / changeColor * 100;
+    let grn = 140 / 100 * pct;
+    return `rgba(255, ${Math.round(grn)}, 0, ${op})`;
+  }
+  if (percent > changeColor && percent <= (changeColor * 2)) {
+    let pct = (percent - changeColor) / changeColor * 100;
+    let grn = 140 + (115 / 100 * pct )
+    return `rgba(255, ${Math.round(grn)}, 0, ${op})`;
+  }
+  if (percent > (changeColor * 2) && percent <= changeColor * 3) {
+    let pct = (percent - changeColor * 2) / changeColor * 100;
+    let red = 255 - (255 / 100 * pct )
+    let grn = 255 - (127 / 100 * pct )
+    return `rgba(${Math.round(red)}, ${Math.round(grn)}, 0, ${op})`;
+    //from RGB(255, 255, 0) to RGB(0, 128, 0)
+  }
+  if (percent > (changeColor * 3) && percent <= changeColor * 4) {
+    let pct = (percent - changeColor * 3) / changeColor * 100;
+    let grn = 128 - (128 / 100 * pct )
+    let blue = (255 / 100 * pct )
+    return `rgba(0, ${Math.round(grn)}, ${Math.round(blue)}, ${op})`;
+  }
+  if (percent > (changeColor * 4) && percent <= changeColor * 5) {//RGB(148, 0, 211)
+    let pct = (percent - changeColor * 4) / changeColor * 100;
+    let red =  (148 / 100 * pct )
+    let blue = 255 - (44 / 100 * pct )
+    return `rgba(${Math.round(red)}, 0, ${Math.round(blue)}, ${op})`;
+  }
+  if (percent > (changeColor * 5) && percent < changeColor * 6) {
+    let pct = (percent - changeColor * 5) / changeColor * 100;
+    let red =  148 + (107 / 100 * pct )
+    let blue = 211 - (211 / 100 * pct )
+    return `rgba(${Math.round(red)}, 0, ${Math.round(blue)}, ${op})`;
+  }
+  return 'rgb(255,0,0)';
+};
+
 const state = reactive({ count: 0, loaded: 0, max: Math.round(Math.random() * 200) })
 </script>
 <style>
 :root {
-  --rad: 20px;
-  --bar-height: 40px;
-  --con-height: 50px;
+  --pct-color: rgb(255,255,255);
+  --rad: 10px;
+  --bar-height: 20px;
+  --con-height: 30px;
   --percentage: 0;
-  --text-shadow: 0 1px 0 #ccc, 
-               0 2px 0 rgb(165, 164, 164),
-               0 3px 0 rgb(121, 121, 121),
-               0 4px 1px rgba(0,0,0,.1),
-               0 0 5px rgba(0,0,0,.1),
-               0 1px 3px rgba(0,0,0,.3),
-               0 3px 5px rgba(0,0,0,.2),
-               0 5px 10px rgba(0,0,0,.25),
-               0 10px 10px rgba(0,0,0,.2),
-               0 20px 20px rgba(0,0,0,.15);
 }
 </style>
 <style scoped>
-a {
-  color: #42b983;
-}
 
 .mybar {
-  background:rgb(calc( 155 - (155 / 100 * var(--percentage))),
-    calc(155 / 100 * var(--percentage)),
-    0);
+  background: var(--pct-color);
   height: var(--bar-height);
   box-shadow: rgb(18, 18, 46) 0px 50px 100px -20px,
     rgb(51, 27, 27) 0px 30px 60px -30px,
     rgba(20, 63, 11, 0.979) 0px -2px 6px 0px inset;
   border-radius: var(--rad);
   color: rgb(255, 253, 253);
+  text-shadow: 1px 1px 1px black;
 }
 
 .mybar-container {
@@ -118,13 +147,7 @@ a {
 }
 
 #msg {
-  font-family: fantasy;
-  background: lightgray;
-  box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px,
-    rgba(240, 127, 127, 0.3) 0px 30px 60px -30px,
-    rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
-  margin: 50px;
-  padding: 5px;
+  font-weight:bold;
 }
 .title-box {
   background-color: #e0dcdc;
@@ -132,19 +155,21 @@ a {
 #loadmsg {
   font-weight:bold;
   font-style: italic;
-  text-shadow: var(--text-shadow);
+  float: left;
 }
 
-.mytitle {
-  font-family: fantasy;
-  font-weight:bold;
+.engrave {
   background-color: #666666;
   -webkit-background-clip: text;
   -moz-background-clip: text;
   background-clip: text;
   color: transparent;
+  text-shadow: rgba(255,255,255,0.5) 0px 1px 2px;
+}
+
+.mytitle {
+  font-weight:bold;
   font-size: 50px;
-  text-shadow: rgba(255,255,255,0.5) 0px 3px 3px;
 }
 </style>
 
